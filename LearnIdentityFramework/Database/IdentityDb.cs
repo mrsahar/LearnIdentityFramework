d6cf8e80e7a80accd8795b69ac717701655edab9
mrsaharg <winsahar5@gmail.com>
2025-07-04 20:25:31 +0500

    Add project files.

---
+++ LearnIdentityFramework/Database/IdentityDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearnIdentityFramework.Database
+{
+    public class IdentityDb : DbContext
+    {
+        public IdentityDb(DbContextOptions options):base(options) { }
+    }
+}
